4a0091ba3ebace7af6e2e6282b4e83a8f3b0c38e
Zachary Moh <zacharymoh11@gmail.com>
2021-09-14 13:29:05 -0500

    Updated server.js

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require("expess");
 const path = require("path");
-const Rollbar = require("rollbar");
+// const Rollbar = require("rollbar");
 
 // let rollbar = new Rollbar({
 //     accessToken: "fill this in with something later",
@@ -12,12 +12,12 @@ const app = express();
 
 app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, "/public/index.html"));
-    rollbar.info("HTML file server success!");
+//     rollbar.info("HTML file server success!");
 })
 
 const port = process.env.PORT || 4444;
 
-app.use(rollbar.errorHandler());
+// app.use(rollbar.errorHandler());
 
 app.listen(port, () => {
     console.log(`Server up and running on ${port}!`);
