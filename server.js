1ca0e72f51fe4d8e884e4719d2a1a4cbe349ca2a
Zachary Moh <zacharymoh11@gmail.com>
2021-09-14 15:25:29 -0500

    update

--- server.js
+++ server.js
@@ -26,7 +26,7 @@ app.listen(port, () => {
     console.log(`Server up and running on ${port}!`);
 })
 
-// const students = [];
+const students = [];
 
 
 app.get('/api/spongebob', (req, res) => {
@@ -37,22 +37,22 @@ app.get('/api/spongebob', (req, res) => {
     }
 })
 
-// app.get('/api/student', (req, res)=>{
-//     let {name} = req.body
-//     name = name.trim()
+app.get('/api/student', (req, res)=>{
+    let {name} = req.body
+    name = name.trim()
 
-//     const index = students.findIndex(studentName=> studentName === name)
+    const index = students.findIndex(studentName=> studentName === name)
 
-//     if(index === -1 && name !== ''){
-//         students.push(name)
-//         rollbar.log('Student added successfully', {author: 'Jeddy', type: 'manual entry'})
-//         res.status(200).send(students)
-//     } else if (name === ''){
-//         rollbar.error('No name given')
-//         res.status(400).send('must provide a name.')
-//     } else {
-//         rollbar.error('student already exists')
-//         res.status(400).send('that student already exists')
-//     }
+    if(index === -1 && name !== ''){
+        students.push(name)
+        rollbar.log('Student added successfully', {author: 'Jeddy', type: 'manual entry'})
+        res.status(200).send(students)
+    } else if (name === ''){
+        rollbar.error('No name given')
+        res.status(400).send('must provide a name.')
+    } else {
+        rollbar.error('student already exists')
+        res.status(400).send('that student already exists')
+    }
 
-// })
+})
