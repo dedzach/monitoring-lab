9a4f55b56a1e433740f9c18b982dffac0196763a
Zachary Moh <zacharymoh11@gmail.com>
2021-09-14 13:57:25 -0500

    update

--- server.js
+++ server.js
@@ -1,23 +1,23 @@
 const express = require("express");
 const path = require("path");
-// const Rollbar = require("rollbar");
+const Rollbar = require("rollbar");
 
-// let rollbar = new Rollbar({
-//     accessToken: "fill this in with something later",
-//     captureUncaught: true,
-//     captureUnhandledRejections: true
-// })
+let rollbar = new Rollbar({
+    accessToken: "ea6f594bbb614999ad553967a21ba9d0",
+    captureUncaught: true,
+    captureUnhandledRejections: true
+})
 
 const app = express();
 
 app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, "/public/index.html"));
-//     rollbar.info("HTML file server success!");
+    rollbar.info("HTML file server success!");
 })
 
 const port = process.env.PORT || 4444;
 
-// app.use(rollbar.errorHandler());
+app.use(rollbar.errorHandler());
 
 app.listen(port, () => {
     console.log(`Server up and running on ${port}!`);
